3c28fc71bb951feba49c531c27a9bf9547bce0a9
Clemsky <clement.chardon@student.uclouvain.be>
2019-04-30 19:20:43 +0200

    Stack

--- CircularLinkedList.c
+++ CircularLinkedList.c
@@ -5,23 +5,41 @@
 #include <string.h>
 #include <stdbool.h>
 
+#include "CircularLinkedList.h"
+
 typedef struct{
   Node *head;
 } CircularLinkedList;
 
 typedef struct {
+  char* password;
   int n_vowels;
   int n_consonants;
   Node *next;
   Node *previous;
 } Node;
 
-int add(CircularLinkedList *list, int vowels, int consonants){
+int add(CircularLinkedList *list, char* s){
+
+  int i = 0;
+  int vowels = 0;
+  int consonants = 0;
+
+  while(s[i] != '\0'){
+    if (s[c] == 'a'|| s[c] == 'e' || s[c] == 'i' || s[c] =='o' || s[c] == 'u' || s[c] == 'y'){
+      vowels++;
+    }
+    else{
+      consonants++;
+    }
+  }
+
   Node* new = (Node*)malloc(sizeof(Node));
   if(new == NULL){
     return 1;
   }
 
+  new->password = s;
   new->n_vowels = vowels;
   new->n_consonants = consonants;
 
@@ -65,6 +83,7 @@ void remove(CircularLinkedList* list, Node* rmv){
 
 void printAll(CircularLinkedList* list){
   while(list->head != NULL){
-    printf("%s\n", );
+    printf("%s\n",list->head->password);
+    remove(list, list->head);
   }
 }
