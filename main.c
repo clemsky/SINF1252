a133110184c855a6f3c30e22c9dbe4222be916d6
Clemsky <clement.chardon@student.uclouvain.be>
2019-05-09 17:34:17 +0200

    Multiple file

--- main.c
+++ main.c
@@ -14,13 +14,33 @@
 int main(int argc, char* argv[]){
 
   int err;
-  
-  int n_threads = 1;
-  FILE* binFile;
-  binFile = (FILE*)malloc(sizeof(FILE));
-  binFile = NULL;
-  int option = VOWELS;
 
+  int n_threads = 1;
+  int option = VOWELS;
+  int numberFiles = 0;
+
+  for (int i = 0; i < argc; i++){
+    const char* str = argv[i];
+    if (strstr(str,".bin") != NULL){
+      numberFiles++;
+    }
+  }
+
+  if(numberFiles == 0){
+    printf("Pas de fichier en argument\n");
+    exit(EXIT_FAILURE);
+  }
+
+  FILE* binFile[numberFiles];
+  for (int i = 0; i < numberFiles; i++){
+    binFile[i] = (FILE*)malloc(sizeof(FILE));
+  }
+  char* fileName[numberFiles];
+  for (int i = 0; i < numberFiles; i++){
+    fileName[i] = (char*)malloc(30*sizeof(char));
+  }
+
+  int j = 0;
   for (int i = 0; i < argc; i++){
     const char* str = argv[i];
     if (strcmp(argv[i],"-t") == 0){
@@ -30,13 +50,15 @@ int main(int argc, char* argv[]){
       option = CONSONANTS;
     }
     if (strstr(str,".bin") != NULL){
-      binFile = fopen(str,"rb");
+      if (strlen(str) > 30){
+        printf("Nom de fichier trop long");
+        exit(EXIT_FAILURE);
+      }
+      strcpy(fileName[j],str);
+      j++;
     }
   }
-  if(binFile == NULL){
-    printf("Pas de file en argument\n");
-    exit(EXIT_FAILURE);
-  }
+
 
   Stack* s;
   s = (Stack*)malloc(sizeof(Stack));
@@ -44,10 +66,6 @@ int main(int argc, char* argv[]){
 
   pthread_t thread[n_threads];
   arg_thread *arg_t[n_threads];
-
-  int numberHashes = 5;
-  int numberHashesPerThread = numberHashes/n_threads;
-  int rest = numberHashes % n_threads;
 
   err = pthread_mutex_init(&mutex_stack, NULL);
   if(err != 0){
@@ -66,32 +84,49 @@ int main(int argc, char* argv[]){
     if(arg_t[i] == NULL){
       exit(EXIT_FAILURE);
     }
-
-    arg_t[i]->file = binFile;
     arg_t[i]->option = option;
-    arg_t[i]->operation = numberHashesPerThread;
+    arg_t[i]->number = i+1;
     arg_t[i]->stack = s;
-    arg_t[i]->number = i;
-    if (rest > 0){
-      arg_t[i]->operation++;
-      rest--;
-    }
   }
 
-  for (int i = 0; i < n_threads; i++){
-    err = pthread_create(&(thread[i]), NULL, &computing, (void*)arg_t[i]);
-    if(err != 0){
-      perror("pthread_create");
-      exit(EXIT_FAILURE);
-    }
-  }
+  for (int a = 0; a < numberFiles; a++){
 
-  for (int i = 0; i < n_threads; i++){
-    err = pthread_join(thread[i], NULL);
-    if(err != 0){
-      perror("pthread_join");
+    printf("Analyse du fichier %i\n",(a+1));
+    binFile[a] = fopen(fileName[a],"rb");
+    if(binFile[a] == NULL){
+      printf("Erreur ouverture fichier\n");
       exit(EXIT_FAILURE);
     }
+
+    fseek(binFile[a], 0L, SEEK_END);
+    int size = ftell(binFile[a]);
+    rewind(binFile[a]);
+
+    int *numberHashes;
+    numberHashes = (int*)malloc(sizeof(int));
+    *numberHashes= size/32;
+
+    for (int i = 0; i < n_threads; i++){
+      arg_t[i]->file = binFile[a];
+      arg_t[i]->numberHashes = numberHashes;
+    }
+
+    for (int i = 0; i < n_threads; i++){
+      err = pthread_create(&(thread[i]), NULL, &compute, (void*)arg_t[i]);
+      if(err != 0){
+        perror("pthread_create");
+        exit(EXIT_FAILURE);
+      }
+    }
+
+    for (int i = 0; i < n_threads; i++){
+      err = pthread_join(thread[i], NULL);
+      if(err != 0){
+        perror("pthread_join");
+        exit(EXIT_FAILURE);
+      }
+    }
+    fclose(binFile[a]);
   }
 
   err = pthread_mutex_destroy(&mutex_stack);
@@ -108,16 +143,16 @@ int main(int argc, char* argv[]){
 
   printf("Les mots de passe candidats sont :\n");
   printAll(s);
-  fclose(binFile);
   return EXIT_SUCCESS;
 }
 
-void* computing(void* arg){
+void* compute(void* arg){
   arg_thread* arg_t = (arg_thread*)arg;
   size_t len = 16;
 
-  for(int i = 0; i < arg_t->operation; i++){
+  while(true){
 
+    bool flag1 = false;
     char buffer[32];
     uint8_t hash[32];
     char* res;
@@ -127,22 +162,29 @@ void* computing(void* arg){
     }
 
     pthread_mutex_lock(&mutex_file);
-    printf("Thread %i : begin fread\n",arg_t->number);
-    fread(buffer,sizeof(buffer),1,arg_t->file);
-    printf("Thread %i : end fread\n",arg_t->number);
+    printf("NumberHashes : %i\n",*(arg_t->numberHashes));
+    if(*(arg_t->numberHashes) > 0){
+      *(arg_t->numberHashes) = *(arg_t->numberHashes)-1;
+      printf("Thread %i : begin fread\n",arg_t->number);
+      fread(buffer,sizeof(buffer),1,arg_t->file);
+      printf("Thread %i : end fread\n",arg_t->number);
+      flag1 = true;
+    }
     pthread_mutex_unlock(&mutex_file);
 
-    /* Calling reversehash function */
+    if(!flag1){
+      break;
+    }
 
     for (int i =0; i<32; i++){
       hash[i] = (uint8_t)buffer[i];
     }
 
     printf("Thread %i : begin revershash\n",arg_t->number);
-    bool flag = reversehash(hash,res,len);
+    bool flag2 = reversehash(hash,res,len);
     printf("Thread %i : end reversehash\n",arg_t->number);
 
-    if(flag){
+    if(flag2){
       pthread_mutex_lock(&mutex_stack);
       place(arg_t->stack,res,arg_t->option);
       pthread_mutex_unlock(&mutex_stack);
--- main.h
+++ main.h
@@ -19,8 +19,8 @@ pthread_mutex_t mutex_stack;
 pthread_mutex_t mutex_file;
 
 typedef struct arg_thread {
-  int operation;
   int number;
+  int* numberHashes;
   FILE *file;
   int option;
   Stack* stack;
@@ -28,6 +28,6 @@ typedef struct arg_thread {
 
 int main(int argc, char *argv[]);
 
-void* computing(void* arg);
+void* compute(void* arg);
 
 #endif
